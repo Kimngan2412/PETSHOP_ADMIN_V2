f72d31bc30a47d022e9ad267228ac07b0afc2a85
Jim <102190276@sv1.dut.udn.vn>
2023-07-13 11:35:08 +0700

    update url api

--- src/components/file-uploader/FileUploaderMultiple.tsx
+++ src/components/file-uploader/FileUploaderMultiple.tsx
@@ -32,7 +32,7 @@ const FileUploaderMultiple = ({ value, onChange }: any) => {
     const convertImageUrlToFile = async (files: any) => {
       const newFiles = []
       for (const file of files) {
-        const imageUrl = `https://api.petshop.hieshop.click/${file?.url}`;
+        const imageUrl = `https://kimngan2412-petshop.onrender.com/${file?.url}`;
         try {
           const response = await axios.get(imageUrl, {
             responseType: 'blob', headers: {
@@ -109,7 +109,7 @@ const FileUploaderMultiple = ({ value, onChange }: any) => {
   return (
     <Fragment>
       {/* <div>
-        <img src="https://api.petshop.hieshop.click/uploads/images-1685593894816-792129722.jpg" alt="" />
+        <img src="https://kimngan2412-petshop.onrender.com/uploads/images-1685593894816-792129722.jpg" alt="" />
       </div> */}
       <div {...getRootProps({ className: 'dropzone' })}>
         <input {...getInputProps()} />
